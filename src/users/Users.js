932861ad8c1b6abd99e10640928bc8985fc38d65
cecep29 <cecepefendi.c2p@gmail.com>
2018-09-20 16:10:58 +0700

    init project

---
+++ src/users/Users.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import User from './User';
+
+
+class Users extends Component {
+  state = {
+    users: [
+      {name: "John", age: 30},
+      {name: "Ucok", age: 20},
+      {name: "Baba", age: 40}
+    ],
+    title: "User List"
+  };
+
+  makeMeYounger = () => {
+    const newState = this.state.users.map((user)=> {
+      const tempUser = user;
+      tempUser.age -= 10;
+      return tempUser
+    });
+
+    this.setState({
+      newState
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <button onClick={this.makeMeYounger}>Make us 10 years younger</button>
+        <br/>
+        <h1>{this.state.title}</h1>
+        {
+          this.state.users.map((user)=>{
+            return <User age={user.age}>{user.name}</User>
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default Users;
